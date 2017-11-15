8e3b3eacdd59ddac2c7190e96c34209bfd4801d5
charlyhd86 <carloshdelgado@outlook.com>
2017-11-15 18:34:22 +0100

    Added option to shot many bullets at a time. Added multi-enemy spawn

--- Practica1/Practica1.cpp
+++ Practica1/Practica1.cpp
@@ -1,24 +1,34 @@
 #include <conio.h>
 #include <stdio.h>
 #include <Windows.h>
+#include <list>
 
 
-const int KEY_ESC      = 27;
-const int KEY_D        = 'D';
-const int KEY_d        = 'd';
-const int KEY_A        = 'A';
-const int KEY_a        = 'a';
-const int KEY_L        = 'L';
-const int KEY_l        = 'l';
-const int KEY_K        = 'K';
-const int KEY_k        = 'k';
-const int LEFT_LIMIT  = 10;
-const int RIGHT_LIMIT  = 40;
-const int ENEMY_RESPAWN_COUNT  = 65;
-const int CHARACTER_RESPAWN_COUNT = 30;
-const int MUSHROOM_RESPAWN_COUNT = 60;
-const int MUSHROOM_POINTS = 50;
+const char KEY_ESC = 27;
+const char KEY_D = 'D';
+const char KEY_d = 'd';
+const char KEY_A = 'A';
+const char KEY_a = 'a';
+const char KEY_L = 'L';
+const char KEY_l = 'l';
+const char KEY_K = 'K';
+const char KEY_k = 'k';
+const int LEFT_LIMIT = 5;
+const int RIGHT_LIMIT = 50;
+const int ENEMY_RESPAWN_COUNT = 15;
+const int CHARACTER_RESPAWN_COUNT = 45;
+const int MUSHROOM_RESPAWN_COUNT = (60 + LEFT_LIMIT) * -1;
+const int MUSHROOM_OBTAINED_POINTS = 50;
 const int MUSHROOM_DESTROYED_POINTS = 10;
+
+
+struct Entity {
+	int position;
+	bool direction; // true to right, false to left
+};
+
+struct Bullet : public Entity {};
+struct Enemy : public Entity {};
 
 /************/
 
@@ -26,127 +36,76 @@ bool isValidPosition(int position) {
 	return (position >= LEFT_LIMIT && position < RIGHT_LIMIT);
 }
 
+bool checkDynamicEntityHit(const Entity &entity, int hitPosition) {
+	int nextPosition = entity.direction ? (entity.position + 1) : (entity.position - 1);
+	return(!(entity.position - hitPosition) || !(nextPosition - hitPosition));
+}
+
 /************/
 
+// TODO Utilizar list o vector de balas para disparar varias balas a la vez, enemigos a la vez, champiñones a la vez.
+
 int main() {
-	int  i;
-	int  keyPressed          = 0;
-	bool doPlay              = true;
-	int  characterPosition   = LEFT_LIMIT;
-	int  bulletPosition      = -1;
-	int  enemyPosition       = (ENEMY_RESPAWN_COUNT + LEFT_LIMIT) * -1;
-	int  mushroomPosition    = (MUSHROOM_RESPAWN_COUNT + LEFT_LIMIT) * -1;
-	bool bulletDirection     = true;                              // true to right, false to left
-	bool enemyDirection      = true;                              // true to right, false to left
-	bool bulletHitsEnemy     = false;
-	bool enemyHitsCharacter  = false;
-	int  score               = 0;
-	int  lives               = 3;
+	int                         i;
+	char                        keyPressed = 0;
+	bool                        doPlay = true;
+	int                         characterPosition = LEFT_LIMIT;
+	int                         mushroomPosition = MUSHROOM_RESPAWN_COUNT;
+	int                         score = 0;
+	int                         lives = 3;
+	int                         loopsToEnemyRespawn = ENEMY_RESPAWN_COUNT;
+	std::list<Bullet>           lstBullets;
+	std::list<Bullet>::iterator itB;
+	std::list<Enemy>            lstEnemies;
+	std::list<Enemy>::iterator  itE;
 
 	printf("\n\n\n\n\n\n");
 
-	while (doPlay && lives) {
-
-		Sleep(50);
-
-		// Input
+	do {
+		// Process input
 		if (kbhit()) {
 			keyPressed = getch();
 			switch (keyPressed) {
-				case KEY_ESC: //ESC
-					doPlay = false;
-					break;
+			case KEY_ESC:
+				doPlay = false;
+				break;
 
-				case KEY_D:
-				case KEY_d:
-					// Move right
-					if(characterPosition >= LEFT_LIMIT && characterPosition < (RIGHT_LIMIT - 1))
-						++characterPosition;
-					break;
+			case KEY_D:
+			case KEY_d:
+				// Move right
+				if (characterPosition >= LEFT_LIMIT && characterPosition < (RIGHT_LIMIT - 1))
+					++characterPosition;
+				break;
 
-				case KEY_A:
-				case KEY_a:
-					// Move left
-					if (characterPosition > LEFT_LIMIT)
-						--characterPosition;
-					break;
+			case KEY_A:
+			case KEY_a:
+				// Move left
+				if (characterPosition > LEFT_LIMIT)
+					--characterPosition;
+				break;
 
-				case KEY_L:
-				case KEY_l:
-					// Shoot right
-					if (bulletPosition < LEFT_LIMIT || bulletPosition >= RIGHT_LIMIT) {
-						bulletPosition = characterPosition;
-						bulletDirection = true;
-					}
-					break;
+			case KEY_L:
+			case KEY_l:
+				// Shoot right
+				Bullet bulletR;
+				bulletR.position = characterPosition;
+				bulletR.direction = true;
+				lstBullets.push_back(bulletR);
+				break;
 
-				case KEY_K:
-				case KEY_k:
-					// Shoot left
-					if (bulletPosition < LEFT_LIMIT || bulletPosition >= RIGHT_LIMIT) {
-						bulletPosition = characterPosition;
-						bulletDirection = false;
-					}
-					break;
+			case KEY_K:
+			case KEY_k:
+				// Shoot left
+				Bullet bulletL;
+				bulletL.position = characterPosition;
+				bulletL.direction = false;
+				lstBullets.push_back(bulletL);
+				break;
 			}
 		}
 
 
 		// Logic
-		if (enemyPosition == characterPosition)
-			enemyHitsCharacter = true;
-
-		if (!enemyDirection) {
-			--enemyPosition;
-			if (bulletDirection && (enemyPosition - bulletPosition) <= 1)
-				bulletHitsEnemy = true;
-		}
-		else {
-			++enemyPosition;
-			if (!bulletDirection && (bulletPosition - enemyPosition) <= 1)
-				bulletHitsEnemy = true;
-		}
-
-		if (isValidPosition(bulletPosition)) {
-			if (bulletDirection)
-				++bulletPosition;
-			else
-				--bulletPosition;
-
-			if (bulletHitsEnemy || enemyPosition == bulletPosition) { // Bullet hits enemy
-				bulletPosition = -1;
-				if (enemyDirection) {
-					enemyPosition = ENEMY_RESPAWN_COUNT + RIGHT_LIMIT;
-					enemyDirection = false;
-				}
-				else {
-					enemyPosition = (ENEMY_RESPAWN_COUNT + LEFT_LIMIT) * -1;
-					enemyDirection = true;
-				}
-
-				enemyHitsCharacter = false;
-				bulletHitsEnemy = false;
-			}
-		}
-
-		if (enemyHitsCharacter || enemyPosition == characterPosition) { // Enemy hits character
-			--lives;
-			if (lives)
-				characterPosition = -1 * CHARACTER_RESPAWN_COUNT;
-
-				enemyPosition = ENEMY_RESPAWN_COUNT + RIGHT_LIMIT;
-			if (enemyDirection) {
-				enemyDirection = false;
-			}
-			else {
-				enemyPosition *= -1;
-				enemyDirection = true;
-			}
-
-			enemyHitsCharacter = false;
-			bulletHitsEnemy = false;
-		}
-
 		if (!isValidPosition(mushroomPosition)) {
 			++mushroomPosition;
 			if (isValidPosition(mushroomPosition)) { // Mushroom appears in a random position.
@@ -154,54 +113,104 @@ int main() {
 			}
 		}
 
-		if (isValidPosition(mushroomPosition)) {
-			if (mushroomPosition == characterPosition) { // Character takes mushroom
-				mushroomPosition = (MUSHROOM_RESPAWN_COUNT + LEFT_LIMIT) * -1;
-				score += MUSHROOM_POINTS;
+		itE = lstEnemies.begin();
+		while (itE != lstEnemies.end()) {
+			if (!itE->direction) --(itE->position);
+			else                 ++(itE->position);
+
+			if (isValidPosition(itE->position) && itE->position == characterPosition) { // Enemy hits character
+				--lives;
+				if (lives) characterPosition = -1 * CHARACTER_RESPAWN_COUNT;
+				itE = lstEnemies.erase(itE);
+			}
+			else
+				++itE;
+		}
+
+		if (isValidPosition(mushroomPosition) && mushroomPosition == characterPosition) { // Character takes mushroom
+			mushroomPosition = MUSHROOM_RESPAWN_COUNT;
+			score += MUSHROOM_OBTAINED_POINTS;
+		}
+
+		itB = lstBullets.begin();
+		while (itB != lstBullets.end()) {
+			bool bulletErased = false;
+			if (isValidPosition(mushroomPosition) && checkDynamicEntityHit(*itB, mushroomPosition)) { // Bullet hits mushroom
+				mushroomPosition = MUSHROOM_RESPAWN_COUNT;
+				score -= MUSHROOM_DESTROYED_POINTS;
+				itB = lstBullets.erase(itB);
+				bulletErased = true;
+			}
+			else {
+				itE = lstEnemies.begin();
+				while (itE != lstEnemies.end()) {
+					if (checkDynamicEntityHit(*itB, itE->position)) { // Bullet hits enemy
+						itE = lstEnemies.erase(itE);
+						itB = lstBullets.erase(itB);
+						bulletErased = true;
+						break;
+					}
+					else
+						++itE;
+				}
 			}
 
-			if (mushroomPosition == bulletPosition) { // Bullet hits mushroom
-				bulletPosition = -1;
-				bulletHitsEnemy = false;
-				mushroomPosition = (MUSHROOM_RESPAWN_COUNT + LEFT_LIMIT) * -1;
-				score -= MUSHROOM_DESTROYED_POINTS;
+			if (!bulletErased) {
+				itB->direction                   ? ++(itB->position) : --(itB->position);
+				(isValidPosition(itB->position)) ? ++itB             : itB = lstBullets.erase(itB);
 			}
 		}
 
-		if (characterPosition < LEFT_LIMIT)
-			characterPosition++;
+		--loopsToEnemyRespawn;
+		if (!loopsToEnemyRespawn) { // Enemy respawn
+			loopsToEnemyRespawn = ENEMY_RESPAWN_COUNT;
+			Enemy newEnemy;
+			newEnemy.direction = rand() % 2;
+			newEnemy.direction ? newEnemy.position = LEFT_LIMIT : newEnemy.position = RIGHT_LIMIT;
+			lstEnemies.push_back(newEnemy);
+		}
+
+		if (!isValidPosition(characterPosition)) characterPosition++; // Character respawning
 
 
-		// Paint elements
+																	  // Render
 		printf("\r");
 		for (i = 0; i <= RIGHT_LIMIT; i++) {
 			if (isValidPosition(i)) {
-				if (i == bulletPosition) {
-					if (bulletDirection)
-						printf(">");
-					else
-						printf("<");
+				bool renderedEntity = false;
+				for (std::list<Enemy>::iterator itE = lstEnemies.begin(); itE != lstEnemies.end(); ++itE) {
+					if (i == itE->position) {
+						printf("@");
+						renderedEntity = true;
+						break;
+					}
 				}
-				else if (i == characterPosition)
-					printf("X");
-				else if (i == enemyPosition)
-					printf("@");
-				else if (i == mushroomPosition)
-					printf(":");
-				else
-					printf("_");
-			} else {
-				if (0 == i)
-					printf("%d", lives);
-				else if (RIGHT_LIMIT == i) {
-					printf("   %d   ", score);
+
+				if (!renderedEntity) {
+					for (std::list<Bullet>::iterator itB = lstBullets.begin(); itB != lstBullets.end(); ++itB) {
+						if (i == itB->position) {
+							if (itB->direction) printf(">");
+							else                printf("<");
+							renderedEntity = true;
+							break;
+						}
+					}
 				}
-				else
-					printf(" ");
+
+				if (!renderedEntity) {
+					if (i == characterPosition)      printf("X");
+					else if (i == mushroomPosition)  printf(":");
+					else                             printf("_");
+				}
+			}
+			else {
+				if (RIGHT_LIMIT == i) printf("   LIVES: %d  SCORE: %d", lives, score);
+				else                  printf(" ");
 			}
 		}
 
-	}
+		Sleep(50);
+	} while (doPlay && lives);
 
 	if (!lives) {
 		printf("\n\n\n\n");
@@ -210,7 +219,7 @@ int main() {
 		getch();
 	}
 
-    return 0;
+	return 0;
 }
 
 
